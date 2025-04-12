2096dda27f51f5c184714545da5f8d42ea63ff9c
Cosmin <cosminrosu.csr@gmail.com>
2025-04-12 10:45:43 +0300

    version 2

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+require('dotenv').config()
+
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/products">products route</a>')
+})
+
+app.use('/products', productsRouter)
+
+//products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 4444
+
+const start = async () => {
+  try{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Port ${port} works!`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose')
 
+//function to connect my app to MongoDB that is used in app.js
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  return mongoose.connect(url)
 }
 
 module.exports = connectDB
